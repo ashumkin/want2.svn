1476571a125d1c8bcbfccbfb8e5f3875f2aeeb97
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-03-15 19:23:43 +0000

    First test structure in place -- passes, but no functionality is written at all.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@9 0fbc77ab-9432-0410-b702-c5701067c881

---
+++ test/DanteUnitTest.pas
@@ -0,0 +1,88 @@
+{ $Id$ }
+{
+--------------------------------------------------------------------------
+Copyright (c) 2001, Chris Morris
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. The names Chris Morris, Dante and the names of contributors to this software
+may not be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------------
+(based on BSD Open Source License)
+}
+unit DanteUnitTest;
+
+interface
+
+uses
+  TestFramework, DanteUnit, SysUtils, JclFileUtils, JclShell;
+
+type
+  TTestDanteUnit = class(TTestCase)
+  private
+    FBuildFile: TextFile;
+    FBuildFileName: string;
+    FDante: TDante;
+    FTestDir: string;
+  public
+    procedure Setup; override;
+    procedure TearDown; override;
+  published
+    procedure TestDanteUnit;
+  end;
+
+implementation
+
+{ TTestDanteUnit }
+
+procedure TTestDanteUnit.Setup;
+begin
+  inherited;
+  FTestDir := ExtractFilePath(ParamStr(0)) + 'test';
+  JclFileUtils.ForceDirectories(FTestDir);
+  FDante := TDante.Create;
+  FBuildFileName := FTestDir + '\build.txt';
+  AssignFile(FBuildFile, FBuildFileName);
+  Rewrite(FBuildFile);
+end;
+
+procedure TTestDanteUnit.TearDown;
+begin
+  CloseFile(FBuildFile);
+  FDante.Free;
+  JclShell.SHDeleteFolder(0, FTestDir, [doSilent]);
+  inherited;
+end;
+
+procedure TTestDanteUnit.TestDanteUnit;
+begin
+  WriteLn(FBuildFile, '');
+  FDante.DoBuild(FBuildFileName);
+end;
+
+initialization
+  RegisterTest('', TTestDanteUnit);
+
+end.
+ 
