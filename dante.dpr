1b57e53bc8313260895c4520d9e43832bc9e0b61
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-03-21 14:32:05 +0000

    Dante.exe now grabs build.txt and processes it. -L actually spits out the license info (including Jcl license reference), though it needs to be More-ed. Bootstrap.bat now calls the dante.exe it builds to build the main dante.exe.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@17 0fbc77ab-9432-0410-b702-c5701067c881

--- dante.dpr
+++ dante.dpr
@@ -1,6 +1,6 @@
 { $Id$ }
 {
---------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 Copyright (c) 2001, Chris Morris
 All rights reserved.
 
@@ -36,36 +36,103 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 program dante;
 
 uses
-  DanteUnit, SysUtils;
+  DanteUnit, SysUtils, clUtilConsole;
+
+const
+  BuildFileName = 'build.txt';
 
 function DanteHeader: string;
 begin
-  Result := 'Dante v0.0.0 Build 0. Build Management tool for Delphi';
+  Result := 'Dante v0.0.0 Build 0. Build Management tool for Delphi'#13#10;
+end;
+
+function License: string;
+const
+  CR = '';  // #13#10 is not necessary because text is exactly 80 col
+begin
+  Result :=
+    '--------------------------------------------------------------------------------'+ CR +
+    'Copyright (c) 2001, Chris Morris                                                '+ CR +
+    'All rights reserved.                                                            '+ CR +
+    '                                                                                '+ CR +
+    'Redistribution and use in source and binary forms, with or without modification,'+ CR +
+    'are permitted provided that the following conditions are met:                   '+ CR +
+    '                                                                                '+ CR +
+    '1. Redistributions of source code must retain the above copyright notice, this  '+ CR +
+    'list of conditions and the following disclaimer.                                '+ CR +
+    '                                                                                '+ CR +
+    '2. Redistributions in binary form must reproduce the above copyright notice,    '+ CR +
+    'this list of conditions and the following disclaimer in the documentation and/or'+ CR +
+    'other materials provided with the distribution.                                 '+ CR +
+    '                                                                                '+ CR +
+    '3. The names Chris Morris, Dante and the names of contributors to this software '+ CR +
+    'may not be used to endorse or promote products derived from this software       '+ CR +
+    'without specific prior written permission.                                      '+ CR +
+    '                                                                                '+ CR +
+    'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''''   '+ CR +
+    'AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE       '+ CR +
+    'IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  '+ CR +
+    'DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE   '+ CR +
+    'FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL      '+ CR +
+    'DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR      '+ CR +
+    'SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER      '+ CR +
+    'CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR'+ CR +
+    'TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF   '+ CR +
+    'THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               '+ CR +
+    '--------------------------------------------------------------------------------'+ CR +
+    '                                                                                '+ CR +
+    '--------------------------------------------------------------------------------'+ CR +
+    'The JCL (JEDI Code Library) used in Dante is governed by the terms of the       '+ CR +
+    'MPL (Mozilla Public License) found at http://www.mozilla.org/MPL/MPL-1.1.html.  '+ CR +
+    '                                                                                '+ CR +
+    'The source code for JCL itself can be found on JEDI Web site:                   '+ CR +
+    'http://delphi-jedi.org/Jedi:CODELIBJCL.                                         '+ CR +
+    '--------------------------------------------------------------------------------';
 end;
 
 const
   SwitchChars = ['-', '/'];
+var
+  ADante: TDante;
 begin
+  WriteLn(DanteHeader);
   if FindCmdLineSwitch('?', SwitchChars, true) or
      FindCmdLineSwitch('h', SwitchChars, true) then
   begin
-    WriteLn(DanteHeader);
     WriteLn('For licensing info, use the -L switch');
     WriteLn;
     WriteLn('Usage:');
     WriteLn('  dante.exe [options]');
     WriteLn;
     WriteLn('Options:');
+    WriteLn('  -h, -H, -?          Displays this help text.');
     WriteLn('  -buildfile [file]   Specifies the build file. Default is');
     WriteLn('                      build.txt (will be build.xml in future)');
   end
   else if FindCmdLineSwitch('L', SwitchChars, false) then
   begin
-    WriteLn(DanteHeader);
-    //WriteLn(License);
+    // need to add More functionality ... going to add it in clUtilConsole
+    WriteLn(License);
   end
   else begin
-    WriteLn('not functional yet.');
+    if FileExists(BuildFileName) then
+    begin
+      ADante := TDante.Create;
+      try
+        try
+          ADante.DoBuild(BuildFileName);
+          WriteLn('Build complete.');
+        except
+          on E: Exception do
+            WriteLn(E.message);
+        end;
+      finally
+        ADante.Free;
+      end;
+    end
+    else
+      // in the future add -find support and -buildfile support
+      WriteLn('Cannot find ' + BuildFileName + '. Must be in current directory.');
   end;
 end.
 
--- make/bootstrap.bat
+++ make/bootstrap.bat
@@ -3,4 +3,6 @@ REM Get a local Dante.exe built so it can then do a full build with itself
 REM Compile -- where's the dcc32? reg.exe from NT res kit could grab it
 REM For now, assume it's in the PATH
 
-dcc32.exe -B -Q -E"." ..\dante.dpr 
+dcc32.exe -B -Q -E"." ..\dante.dpr
+
+dante.exe
