fbed565dab70c2f5a463181c19e2399a3cedffab
juanco <juanco@0fbc77ab-9432-0410-b702-c5701067c881>
2001-03-28 22:23:12 +0000

    Added basic logging facility in TLoggerTask, as described in the Task Writers Guide.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@75 0fbc77ab-9432-0410-b702-c5701067c881

---
+++ src/tasks/LoggerTask.pas
@@ -0,0 +1,128 @@
+{ $Id$ }
+{
+--------------------------------------------------------------------------
+Copyright (c) 2001, Juancarlo Añez, Caracas, Venezuela.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. The names Chris Morris, Dante and the names of contributors to this software
+may not be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------------
+(based on BSD Open Source License)
+}
+unit LoggerTask;
+interface
+uses
+  SysUtils,
+  Classes,
+  DanteClasses;
+
+type
+  TLogFormat = (brief, normal, detailed);
+
+  TInfoElement = class(TDanteElement)
+  protected
+    FCode :Integer;
+    FText :string;
+  published
+    property code :Integer read FCode write FCode;
+    property text :string  read FText write FText;
+  end;
+
+  TLoggerTask = class(TTask)
+  protected
+    FFile   :string;
+    FFormat :TLogFormat;
+    FInfos  :TList;
+  public
+    constructor Create(Owner :TDanteElement); override;
+    destructor Destroy; override;
+    class function XMLTag :string; override;
+    procedure Execute; override;
+  published
+    function CreateInfo :TInfoElement;
+
+    property _file  :string     read FFile   write FFile;
+    property format :TLogFormat read FFormat write FFormat;
+  end;
+
+implementation
+
+class function TLoggerTask.XMLTag :string;
+begin
+  Result := 'log';
+end;
+
+constructor TLoggerTask.Create(Owner: TDanteElement);
+begin
+  inherited Create(Owner);
+end;
+
+destructor TLoggerTask.Destroy;
+begin
+  // no need to free the TInfoElements themselves
+  FInfos.Free;
+  inherited Destroy;
+end;
+
+function TLoggerTask.CreateInfo :TInfoElement;
+begin
+  Result := TInfoElement.Create(Self);
+  if FInfos = nil then
+    FInfos := TList.Create;
+  FInfos.Add(Result);
+end;
+
+procedure TLoggerTask.Execute;
+var
+  LogFile: System.Text;
+  i:   Integer;
+begin
+  Log(SysUtils.Format('writing log info to "%s"', [_file]));
+  AboutToScratchPath(_file);
+  System.Assign(LogFile, ToSystemPath(_file));
+  try
+    if FileExists(ToSystemPath(_file)) then
+      System.Append(LogFile)
+    else
+      System.Rewrite(LogFile);
+    try
+      for i := 0 to FInfos.Count-1 do
+      begin
+        with TInfoElement(FInfos[i]) do
+          Writeln(LogFile, DateTimeToStr(Now),
+                           SysUtils.Format(' (%d) %s', [code, text]));
+      end;
+    finally
+      System.Close(LogFile);
+    end;                           
+  except
+    TaskFailure('could not open log file');
+  end;
+end;
+
+initialization
+  RegisterTask(TLoggerTask);
+end.
+
