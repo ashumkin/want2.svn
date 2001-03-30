e2c71bb84a9c2e6eece49bcf51503a1ab3862473
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-03-30 03:46:35 +0000

    Updated license boilerplate in most source units. Added license.txt and authors.txt to doc folder. Copyright now assigned to names in authors.txt.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@102 0fbc77ab-9432-0410-b702-c5701067c881

--- src/ScriptRunner.pas
+++ src/ScriptRunner.pas
@@ -1,7 +1,7 @@
 { $Id$ }
 {
---------------------------------------------------------------------------
-Copyright (c) 2001, Chris Morris
+--------------------------------------------------------------------------------
+Copyright (c) 2001, Dante Authors -- See authors.txt for complete list
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -14,9 +14,9 @@ list of conditions and the following disclaimer.
 this list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
 
-3. The names Chris Morris, Dante and the names of contributors to this software
-may not be used to endorse or promote products derived from this software
-without specific prior written permission.
+3. The name Dante, the names of the authors in authors.txt and the names of
+other contributors to this software may not be used to endorse or promote
+products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -29,7 +29,6 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --------------------------------------------------------------------------------
-(based on BSD Open Source License)
 }
 unit DanteMain;
 
--- src/dante.dpr
+++ src/dante.dpr
@@ -1,7 +1,7 @@
 { $Id$ }
 {
 --------------------------------------------------------------------------------
-Copyright (c) 2001, Chris Morris
+Copyright (c) 2001, Dante Authors -- See authors.txt for complete list
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -14,9 +14,9 @@ list of conditions and the following disclaimer.
 this list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
 
-3. The names Chris Morris, Dante and the names of contributors to this software
-may not be used to endorse or promote products derived from this software
-without specific prior written permission.
+3. The name Dante, the names of the authors in authors.txt and the names of
+other contributors to this software may not be used to endorse or promote
+products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -29,7 +29,6 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --------------------------------------------------------------------------------
-(based on BSD Open Source License)
 }
 
 {$APPTYPE CONSOLE}
@@ -55,7 +54,7 @@ const
 begin
   Result :=
     '--------------------------------------------------------------------------------'+ CR +
-    'Copyright (c) 2001, Chris Morris                                                '+ CR +
+    'Copyright (c) 2001, Dante Authors -- See authors.txt for complete list          '+ CR +
     'All rights reserved.                                                            '+ CR +
     '                                                                                '+ CR +
     'Redistribution and use in source and binary forms, with or without modification,'+ CR +
@@ -68,11 +67,11 @@ begin
     'this list of conditions and the following disclaimer in the documentation and/or'+ CR +
     'other materials provided with the distribution.                                 '+ CR +
     '                                                                                '+ CR +
-    '3. The names Chris Morris, Dante and the names of contributors to this software '+ CR +
-    'may not be used to endorse or promote products derived from this software       '+ CR +
-    'without specific prior written permission.                                      '+ CR +
+    '3. The name Dante, the names of the authors in authors.txt and the names of     '+ CR +
+    'other contributors to this software may not be used to endorse or promote       '+ CR +
+    'products derived from this software without specific prior written permission.  '+ CR +
     '                                                                                '+ CR +
-    'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''''   '+ CR +
+    'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''     '+ CR +
     'AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE       '+ CR +
     'IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  '+ CR +
     'DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE   '+ CR +
