70603b7c60df311041bc60ee620cdeb1f2cb8d18
juanco <juanco@0fbc77ab-9432-0410-b702-c5701067c881>
2001-03-30 05:44:56 +0000

    Added TDanteTask and TEchoTask.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@103 0fbc77ab-9432-0410-b702-c5701067c881

---
+++ src/tasks/EchoTasks.pas
@@ -0,0 +1,94 @@
+{ $Id$ }
+{
+--------------------------------------------------------------------------
+Copyright (c) 2001, Juancarlo Añez, Caracas, Venezuela.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. The names Chris Morris, Dante and the names of contributors to this software
+may not be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------------
+(based on BSD Open Source License)
+}
+unit EchoTasks;
+
+interface
+uses
+  DanteClasses,
+  WildPaths,
+  FileSets,
+  FileOps,
+
+  SysUtils,
+  Classes;
+
+type
+  TEchoTask = class(TTask)
+  protected
+    FMessage :string;
+    FText    :string;
+    FFile    :string;
+    FAppend  :boolean;
+
+  public
+    procedure Execute; override;
+  published
+    property _message :string  read FMessage write FMessage;
+    property _text    :string  read FText    write FText;
+    property _file    :string  read FFile    write FFile;
+    property append   :boolean read FAppend  write FAppend;
+  end;
+
+implementation
+
+{ TEchoTask }
+
+procedure TEchoTask.Execute;
+var
+  EchoFile: System.Text;
+begin
+  if _file = '' then
+    Log(_message + _text)
+  else
+  begin
+    Log(SysUtils.Format('echo to "%s"', [_file]));
+    AboutToScratchPath(_file);
+
+    System.Assign(EchoFile, ToSystemPath(_file));
+    if append and FileExists(ToSystemPath(_file)) then
+      System.Append(EchoFile)
+    else
+      System.Rewrite(EchoFile);
+    try
+      Writeln( EchoFile, _message + _text);
+    finally
+      System.Close(EchoFile);
+    end;
+  end;
+end;
+
+
+initialization
+ RegisterTask(TEchoTask);
+end.
---
+++ src/tasks/WantTasks.pas
@@ -0,0 +1,167 @@
+{ $Id$ }
+{
+--------------------------------------------------------------------------
+Copyright (c) 2001, Juancarlo Añez, Caracas, Venezuela.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. The names Chris Morris, Dante and the names of contributors to this software
+may not be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------------
+(based on BSD Open Source License)
+}
+unit DanteTasks;
+
+interface
+uses
+  DanteClasses,
+  WildPaths,
+  FileSets,
+  FileOps,
+
+  SysUtils,
+  Classes;
+
+type
+  TSubProjectPropertyElement = class(TDanteElement)
+  public
+    class function XMLTag :string;              override;
+    procedure SetProperty(Name, Value :string); override;
+  end;
+
+  TCustomDanteTask = class(TTask)
+  protected
+    FTarget     :string;
+  public
+    property _target   :string read FTarget    write FTarget;
+  end;
+
+  TDanteTask = class(TCustomDanteTask)
+  protected
+    FBuildFile  :string;
+    FDir        :string;
+
+    FSubProject :TProject;
+
+  public
+    constructor Create(Owner: TDanteElement = nil); override;
+    destructor  Destroy; override;
+
+    procedure Validate; override;
+    procedure Execute;  override;
+  published
+    function CreateProperty :TPropertyElement; override;
+
+    property _target;
+    property buildfile :string read FBuildFile write FBuildFile;
+    property dir       :string read FDir       write FDir;
+  end;
+
+  TDanteCallTask = class(TCustomDanteTask)
+  public
+    procedure Validate; override;
+    procedure Execute;  override;
+  published
+    property _target;
+  end;
+
+
+implementation
+
+{ TDanteTask }
+
+constructor TDanteTask.Create(Owner: TDanteElement);
+begin
+  inherited Create(Owner);
+  FSubProject := TProject.Create;
+  FSubProject.OnLog := Self.Log;
+  FSubProject.Properties := Self.Project.Properties;
+  FSubProject.SetInitialBaseDir('');
+
+  buildfile := DanteClasses.BuildFileName;
+end;
+
+destructor TDanteTask.Destroy;
+begin
+  FSubProject.Free;
+  inherited Destroy;
+end;
+
+
+procedure TDanteTask.Validate;
+begin
+  inherited Validate;
+  // nothing required
+end;
+
+procedure TDanteTask.Execute;
+begin
+  // this allows the sub project to inherit paths
+  // calculated by the current project
+  FSubProject.RunPath := ToAbsolutePath(Project.RunPath);
+  if dir <> '' then
+    FSubProject.SetInitialBaseDir(dir);
+
+  Log('building "%s" in directory "%s"', [
+                     ToRelativePath(buildfile),
+                     ToRelativePath(dir)
+                     ]);
+
+
+  FSubProject.LoadXML(buildfile);
+  FSubProject.Build(_target);
+end;
+
+function TDanteTask.CreateProperty: TPropertyElement;
+begin
+  Result := FSubProject.CreateProperty;
+end;
+
+{ TSubProjectPropertyElement }
+
+class function TSubProjectPropertyElement.XMLTag: string;
+begin
+  Result := 'property';
+end;
+
+procedure TSubProjectPropertyElement.SetProperty(Name, Value: string);
+begin
+  (Owner as TDanteTask).FSubProject.SetProperty(Name, Value);
+end;
+
+{ TDanteCallTask }
+
+procedure TDanteCallTask.Validate;
+begin
+  RequireAttribute('target', _target);
+end;
+
+procedure TDanteCallTask.Execute;
+begin
+  Project.Build(_target);
+end;
+
+initialization
+ RegisterTasks([TDanteTask,TDanteCallTask]);
+end.
