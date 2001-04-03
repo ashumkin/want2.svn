cc6b1a916c709abfa4768403dd905b55c6c1a85a
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-04-03 03:23:11 +0000

    Updated license for display to include my personal cLabs BSD license copyright since Dante uses a couple of my units
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@116 0fbc77ab-9432-0410-b702-c5701067c881

--- src/dante.dpr
+++ src/dante.dpr
@@ -89,7 +89,39 @@ begin
     '                                                                                '+ CR +
     'The source code for JCL itself can be found on JEDI Web site:                   '+ CR +
     'http://delphi-jedi.org/Jedi:CODELIBJCL.                                         '+ CR +
+    '--------------------------------------------------------------------------------'+ CR +
+    '                                                                                '+ CR +
+    'Portions include:                                                               '+ CR +
+    '--------------------------------------------------------------------------------'+ CR +
+    'Copyright (c) 2000, Chris Morris                                                '+ CR +
+    'All rights reserved.                                                            '+ CR +
+    '                                                                                '+ CR +
+    'Redistribution and use in source and binary forms, with or without modification,'+ CR +
+    'are permitted provided that the following conditions are met:                   '+ CR +
+    '                                                                                '+ CR +
+    '1. Redistributions of source code must retain the above copyright notice, this  '+ CR +
+    'list of conditions and the following disclaimer.                                '+ CR +
+    '                                                                                '+ CR +
+    '2. Redistributions in binary form must reproduce the above copyright notice,    '+ CR +
+    'this list of conditions and the following disclaimer in the documentation and/or'+ CR +
+    'other materials provided with the distribution.                                 '+ CR +
+    '                                                                                '+ CR +
+    '3. The names Chris Morris, cLabs and the names of contributors to this          '+ CR +
+    'software may be used to endorse or promote products derived from this software  '+ CR +
+    'without specific prior written permission.                                      '+ CR +
+    '                                                                                '+ CR +
+    'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''     '+ CR +
+    'AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE       '+ CR +
+    'IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  '+ CR +
+    'DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE   '+ CR +
+    'FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL      '+ CR +
+    'DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR      '+ CR +
+    'SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER      '+ CR +
+    'CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR'+ CR +
+    'TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF   '+ CR +
+    'THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               '+ CR +
     '--------------------------------------------------------------------------------';
+
 end;
 
 const
