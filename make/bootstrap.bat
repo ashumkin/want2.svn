3ecbd62f7d1e35a217f6e69952fbe4824e9d6153
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-04-03 13:57:28 +0000

    Updated bootstrap.bat and build.xml to use the incverrc file for auto-incrementing the build number and then run brcc32 to update the dantever.res. Not in a final state yet, but working right now.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@127 0fbc77ab-9432-0410-b702-c5701067c881

--- make/bootstrap.bat
+++ make/bootstrap.bat
@@ -4,6 +4,8 @@ REM Get a local Dante.exe built so it can then do a full build with itself
 REM Compile -- where's the dcc32? reg.exe from NT res kit could grab it
 REM For now, assume it's in the PATH
 
+brcc32 ..\src\dantever.rc
+
 dcc32 -B -Q -N%TEMP% -E%TEMP% ..\src\dante.dpr -U..\src;..\src\tasks;..\src\jcl;..\src\paths;..\src\xml;..\src\zip;..\lib\paszlib;..\lib\paszlib\minizip
 if ERRORLEVEL 1 goto ERROR
 %TEMP%\dante.exe
