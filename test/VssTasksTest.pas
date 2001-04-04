3c0c5732aa54e0a024ee32bea8f20ed09b02b3af
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-04-04 16:26:16 +0000

    Latest test -- now tests for label retrieval -- although other problems have prevented me from running it to completion to see if it passes yet. This is just a temporary checkin.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@150 0fbc77ab-9432-0410-b702-c5701067c881

--- test/VssTasksTest.pas
+++ test/VssTasksTest.pas
@@ -41,9 +41,18 @@ uses
 type
   TTestVssTasksBase = class(TTestDirCase)
   protected
+    FRevOneContents: string;
+    FRevTwoContents: string;
     FSampleFileName: string;
+    FSamplePathFileName: string;
     FUserPwd: string;
     FVssPath: string;
+
+    function CompareSampleContents(Contents: string): boolean;
+    procedure MakeSample(Contents: string);
+    procedure MakeSampleRevOne;
+    procedure MakeSampleRevTwo;
+    function ReadSampleContents: string;
   public
     procedure Setup; override;
     procedure TearDown; override;
@@ -57,11 +66,58 @@ type
     procedure TearDown; override;
   published
     procedure TestVssGetTask;
+    procedure TestVssGetTaskLabel;
   end;
 
 implementation
 
 { TTestVssTasksBase }
+
+function TTestVssTasksBase.CompareSampleContents(
+  Contents: string): boolean;
+begin
+  Result := (Contents = ReadSampleContents);
+end;
+
+procedure TTestVssTasksBase.MakeSample(Contents: string);
+var
+  F: TextFile;
+begin
+  AssignFile(F, FSamplePathFileName);
+  Rewrite(F);
+  WriteLn(F, Contents);
+  CloseFile(F);
+end;
+
+procedure TTestVssTasksBase.MakeSampleRevOne;
+begin
+  FRevOneContents := 'rev one contents';
+  MakeSample(FRevOneContents);
+end;
+
+procedure TTestVssTasksBase.MakeSampleRevTwo;
+begin
+  FRevTwoContents := 'rev Two contents';
+  MakeSample(FRevTwoContents);
+end;
+
+function TTestVssTasksBase.ReadSampleContents: string;
+var
+  F: TextFile;
+  ReadContents: string;
+  ReadLine: string;
+begin
+  AssignFile(F, FSamplePathFileName);
+  Reset(F);
+  ReadContents := '';
+  while not Eof(F) do
+  begin
+    ReadLn(F, ReadLine);
+    ReadContents := ReadContents + ReadLine;
+  end;
+  Result := ReadContents;
+  CloseFile(F);
+end;
 
 procedure TTestVssTasksBase.Setup;
 begin
@@ -75,22 +131,28 @@ begin
       specific installation. Make sure the VssPath is set to an empty project }
     FVssPath := '$/Front Office Development/DanteVssTaskTest';
     FUserPwd := 'chrism,saltnpepper';
-    
-    FSampleFileName := MakeSampleTextFile;
+
+    FSamplePathFileName := MakeSampleTextFile;
+    FSampleFileName := ExtractFileName(FSamplePathFileName);
     WinExec32AndWait('ss CP "' + FVssPath + '" -I- -Y' + FUserPwd, 0);
-    WinExec32AndWait('ss Add "' + FSampleFileName + '" -I- -Y' + FUserPwd, 0);
-    if FSampleFileName <> '' then
-      JclShell.SHDeleteFiles(0, FSampleFileName, [doSilent, doAllowUndo]);
+    WinExec32AndWait('ss Add "' + FSamplePathFileName + '" -I- -Y' + FUserPwd, 0);
+    if FSamplePathFileName <> '' then
+      JclShell.SHDeleteFiles(0, FSamplePathFileName, [doSilent, doAllowUndo]);
   {$ENDIF}
 end;
 
 procedure TTestVssTasksBase.TearDown;
 begin
   {$IFDEF RUN_VSS_TESTS}
-    { if you want to purge these test files, you'll have to do that manually }
+    { if you want to purge these test files, you'll have to do that manually.
+      Some of the tests run cleaner if the test file was purged, because
+      repeated deletes don't do anything. However, when I wrote this I did
+      not have access to a test SourceSafe database that could survive an
+      accidental purging while writing this fixture code. -- Chrismo }
     WinExec32AndWait('ss CP "' + FVssPath + '" -I- -Y' + FUserPwd, 0);
-    WinExec32AndWait('ss Delete "' + ExtractFileName(FSampleFileName) +
-      '" -I- -Y' + FUserPwd, 0);
+    if FSampleFileName <> '' then
+      WinExec32AndWait('ss Delete "' + FVssPath + '/' + FSampleFileName +
+        '" -I- -Y' + FUserPwd, 0);
   {$ENDIF}
   inherited;
 end;
@@ -100,7 +162,8 @@ end;
 procedure TTestVssGetTask.Setup;
 begin
   inherited;
-  FVssGetTask := TVssGetTask.Create(nil);
+  FProject := TProject.Create;
+  FVssGetTask := TVssGetTask.Create(FProject.AddTarget('test'));
 end;
 
 procedure TTestVssGetTask.TearDown;
@@ -115,18 +178,48 @@ begin
     FVssGetTask.Validate;
     Fail('validation should have failed');
   except
-    on EDanteError do
-      { nada }
+    on EDanteError do { nada }
   end;
-  
+
   FVssGetTask.VssPath := FVssPath;
   FVssGetTask.Validate;
-  
+
   FVssGetTask.Login := FUserPwd;
   FVssGetTask.LocalPath := FTestDir;
-  FVssGetTask.FileName := ExtractFileName(FSampleFileName);
   FVssGetTask.Execute;
-  Check(FileExists(FSampleFileName), 'file not retrieved');
+  Check(FileExists(FSamplePathFileName), 'file not retrieved');
+end;
+
+procedure TTestVssGetTask.TestVssGetTaskLabel;
+var
+  LabelName: string;
+begin
+  { because the TearDown doesn't purge the test file (for reasons stated there),
+    and after the 1st time its run can't delete the file without purging the
+    previous one, the test below won't actually be operating against a clean
+    file. The label command, run subsequent times, will fail because of the
+    previous labelling in the test file's SS history. However, this test still
+    does its job properly. If you want a clean, clean test, do a purge of the
+    test file first. }
+  ChDir(FTestDir);
+  WinExec32AndWait('ss Checkout ' + FSampleFileName + ' -GL"' + FTestDir + '"' +
+    ' -Y' + FUserPwd, 0);
+  MakeSampleRevOne;
+  WinExec32AndWait('ss Checkin ' + FSampleFileName + ' -CComment -K ' +
+    ' -Y' + FUserPwd, 0);
+  LabelName := 'RevOne';
+  WinExec32AndWait('ss Label "' + FVssPath + '" -CComment -L' + LabelName +
+    ' -I- -Y' + FUserPwd, 0);
+  MakeSampleRevTwo;
+  WinExec32AndWait('ss Checkin ' + FSampleFileName + ' -CComment ' +
+    ' -Y' + FUserPwd, 0);
+
+  FVssGetTask.VssPath := FVssPath;
+  FVssGetTask.Login := FUserPwd;
+  FVssGetTask.LocalPath := FTestDir;
+  FVssGetTask._Label := LabelName;
+  FVssGetTask.Execute;
+  CheckEquals(FRevOneContents, ReadSampleContents, 'wrong version retrieved');
 end;
 
 initialization
