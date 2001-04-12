443808794fd27cc1afb3cdcb59d630f33dff8c0b
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-04-12 21:04:27 +0000

    Added what ended up being a lot of comments on why ToRelativePaths requires 2 absolute paths and why ToRelativePaths works with relative paths but probably shouldn't.
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@229 0fbc77ab-9432-0410-b702-c5701067c881

--- test/WildPathsTest.pas
+++ test/WildPathsTest.pas
@@ -35,28 +35,28 @@ var
   Path: string;
   Base: string;
 begin
-  Path := '/dev/Borland/Delphi5/bin/dcc32.exe';
   Base := '/SF-dante/dante/src';
+  Path := '/dev/Borland/Delphi5/bin/dcc32.exe';
   CheckEquals('../../..' + Path, WildPaths.ToRelativePath(Path, Base),
     'ToRelativePath - Path not in Base');
 
-  Path := '/SF-dante/dante/src/tasks';
   Base := '/SF-dante/dante/src';
+  Path := '/SF-dante/dante/src/tasks';
   CheckEquals('./tasks', WildPaths.ToRelativePath(Path, Base),
     'ToRelativePath - Path subdir');
 
-  Path := '/SF-dante/dante/src';
   Base := '/SF-dante/dante/src';
+  Path := '/SF-dante/dante/src';
   CheckEquals('.', WildPaths.ToRelativePath(Path, Base),
     'ToRelativePath - Path = Base');
 
-  Path := '/SF-dante/dante/src';
   Base := '/dante/src';
+  Path := '/SF-dante/dante/src';
   CheckEquals('../..' + Path, WildPaths.ToRelativePath(Path, Base),
     'ToRelativePath - Path not in Base');
 
-  Path := '/SF-dante';
   Base := '/SF-dante/dante/src';
+  Path := '/SF-dante';
   CheckEquals('../../', WildPaths.ToRelativePath(Path, Base),
     'ToRelativePath - Path parent of Base');
 
@@ -66,7 +66,57 @@ begin
     'ToRelativePath with different drive absolute');
 
   { any case like previous where drive letter was not in same place in both
-    paths? }   
+    paths? -- shouldn't be }
+
+  (* If you remove the checks in ToRelativePath ensuring both Path and Base
+     are already absolute paths, then a lot of these tests below work ... but
+     this means ToRelativePath is assuming that any relative markers (..) in
+     a path represent the same thing, of which there's no guarantee, and this
+     could deceive code using this method.
+
+     However, this assumptive behavior could be useful as in the first test 
+     below. If I know that the up (..) placeholders are equivalent (represent 
+     the same absolute), then I could use ToRelativePath successfully to get 
+     ./testA/AStuff. Otherwise, I have to go the extra step of replacing .. 
+     before calling ToRelativePath. However, some cases are more obviously 
+     arbitrary than others. If the following was implemented, there'd have to be 
+     some way of forcing the user to state what's common ... but that's probably 
+     as much or more work than simply forcing them to use Absolute paths. 
+     -- Chrismo
+
+  Base := '../sample/test';
+  Path := '../sample/test/testA/AStuff';
+  CheckEquals('./testA/AStuff', WildPaths.ToRelativePath(Path, Base),
+    'relative base and path, path subdir of base');
+
+  == Additional examples of assumptive relative behavior
+
+  // Works
+  Base := '../sample/test';
+  Path := '../sample/tst/testA/AStuff';
+  CheckEquals('../tst/testA/AStuff', WildPaths.ToRelativePath(Path, Base),
+    'relative base and path, path on different branch of base');
+
+  // Works
+  Base := '../sample/test';
+  Path := '/tst/testA/AStuff';
+  CheckEquals('/tst/testA/AStuff', WildPaths.ToRelativePath(Path, Base),
+    'relative base, absolute path, path not in base');
+
+  // Works
+  Base := '../sample/test';
+  Path := '/tst/testA/AStuff';
+  CheckEquals('/tst/testA/AStuff', WildPaths.ToRelativePath(Path, Base),
+    'relative base, absolute path, path not in base');
+
+  // Doesn't work
+  Base := '/sample/test';
+  Path := '../test/testA/AStuff';
+  { Absolute of Path could be /sample/test/testA/AStuff OR
+    /different/yuk/test/testA/AStuff OR
+    anything, can't assume the two 'test' directories are on the same path }
+  CheckEquals('../test/testA/AStuff', WildPaths.ToRelativePath(Path, Base),
+    'absolute base, relative path, cannot determine if Path in Base or not');*)
 end;
 
 initialization
