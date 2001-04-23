4c9d23155d20d9ad6d35a2db11ab21492af448f7
chrismo <chrismo@0fbc77ab-9432-0410-b702-c5701067c881>
2001-04-23 15:41:18 +0000

    Updated bootstrap and build.xml to use conditional compile directives in test.dprs .
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@264 0fbc77ab-9432-0410-b702-c5701067c881

--- make/bootstrap.bat
+++ make/bootstrap.bat
@@ -1,12 +1,11 @@
 @ECHO OFF
 REM Get a local Dante.exe built so it can then do a full build with itself
-
-REM Compile -- where's the dcc32? reg.exe from NT res kit could grab it
-REM For now, assume it's in the PATH
+REM brcc32 and dcc32 must be in PATH already
 
 brcc32 ..\src\dantever.rc
 
 dcc32 -B -Q -N%TEMP% -E%TEMP% ..\src\dante.dpr -U..\src;..\src\lib;..\src\tasks;..\lib\jcl;..\lib\xml;..\lib\paszlib;..\lib\paszlib\minizip;..\src\elements;..\lib\perlre
+
 if ERRORLEVEL 1 goto ERROR
 %TEMP%\dante.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto END
