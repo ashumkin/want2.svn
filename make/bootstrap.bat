7cfda3822e6895938d453eab89065c5b5a1e6fb2
juanco <juanco@0fbc77ab-9432-0410-b702-c5701067c881>
2001-04-29 16:45:43 +0000

    Made dcc32 call in bootstrap.bat again be quiet
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@313 0fbc77ab-9432-0410-b702-c5701067c881

--- make/bootstrap.bat
+++ make/bootstrap.bat
@@ -4,8 +4,9 @@ REM brcc32 and dcc32 must be in PATH already
 
 brcc32 ..\src\dantever.rc
 
-dcc32 -B -Q -N%TEMP% -E%TEMP% ..\src\dante.dpr -U..\src;..\src\lib;..\src\tasks;..\lib\jcl;..\lib\xml;..\lib\paszlib;..\lib\paszlib\minizip;..\src\elements;..\lib\perlre
-
+cd ..\src
+dcc32 -Q -B -N%TEMP% -E%TEMP% ..\src\dante.dpr -Ulib;tasks;elements;..\lib\jcl;..\lib\xml;..\lib\paszlib;..\lib\perlre
+cd ..\make
 if ERRORLEVEL 1 goto ERROR
 %TEMP%\dante.exe -color %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto END
