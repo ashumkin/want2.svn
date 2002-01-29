8f307ae74ef35ba7951fed6bf3ed045ae30127aa
juanco <juanco@0fbc77ab-9432-0410-b702-c5701067c881>
2002-01-29 15:29:34 +0000

    release 0.1.115
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@544 0fbc77ab-9432-0410-b702-c5701067c881

--- make/bootstrap.bat
+++ make/bootstrap.bat
@@ -8,7 +8,7 @@ brcc32 -r ..\src\license.rc
 if ERRORLEVEL 1 goto ERROR
 
 cd ..\src
-dcc32 -Q -B -N%TEMP% -E%TEMP% -$O- ..\src\want.dpr -Ulib;tasks;elements;..\lib\jcl;..\lib\perlre -U..\lib\dunit -U..\lib\jal\src;..\lib\jal\lib\paszlib -O..\lib\perlre
+dcc32 -Q -B -N%TEMP% -E%TEMP% -$O- -$J+ ..\src\want.dpr -Ulib;tasks;elements;..\lib\jcl;..\lib\perlre -U..\lib\dunit -U..\lib\jal\src;..\lib\jal\lib\paszlib -O..\lib\perlre
 if ERRORLEVEL 1 goto ERROR
 %TEMP%\want.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 if ERRORLEVEL 1 goto ERROR
