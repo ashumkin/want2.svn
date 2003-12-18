5be3a0268e7afaa9db2f684cfd50b5f6622f465d
hippoman <hippoman@0fbc77ab-9432-0410-b702-c5701067c881>
2003-12-18 17:32:25 +0000

    UseVerbose is an uknown directive for dcc task, commented
    git-svn-id: svn://svn.code.sf.net/p/want/code/trunk@758 0fbc77ab-9432-0410-b702-c5701067c881

--- dof2want/dofCompilerFlags.pas
+++ dof2want/dofCompilerFlags.pas
@@ -115,10 +115,12 @@ begin
           end;
         end;
     end;
+{ Todo: This emits an uknown directive
   if fvalues.Count > 0 then
     begin
       fvalues.Values['UseVerbose'] := BoolToStr[True];
-    end;  
+    end;
+}
   try
     flagValue := freader.sectionValues.Values[dof_ShowWarnings];       
     flagState := Boolean(StrToInt(flagValue));
